9bb1c71cbc8001b1b7fb0515320e785fe0dbdf82
RingZero <mattia.paccamiccio@gmail.com>
2024-06-04 15:18:16 +0200

    trying to define a bot, it compiles

--- lib/fixpointlogic/iterate.ml
+++ lib/fixpointlogic/iterate.ml
@@ -3,23 +3,19 @@ let wStack = Stackwidening.wStack
 module SCG = Scg.SCC
 module Value = Memories.Frame
 
-let wVal _ _ = failwith ""
+let wVal ms1 ms2 = Value.widen ms1 ms2
 
 let rec iterate funcs call stack cache1 evalf =
   let stackWidened, callWidened = wStack stack call in
   let valNew, cache2, scg = evalf funcs callWidened stackWidened cache1 in
-  Format.print_string "\n Iterate: ~New:";
-  Apron.Interval.print Format.std_formatter valNew;
   if SCG.mem callWidened scg then (
     Format.print_string "SCG";
     let valOld =
       if Cache.Cache.mem callWidened cache2 then
         snd (Cache.Cache.find callWidened cache2)
-      else failwith "Value.bot"
+      else Value.bot
     in
     let valWidened = wVal valOld valNew in
-    Format.print_string "\n Result: ";
-    Apron.Interval.print Format.std_formatter valWidened;
 
     let stable =
       if Value.leq valWidened valOld && SCG.cardinal scg = 1 then
