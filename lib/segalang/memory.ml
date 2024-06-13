3ea49cafd090199eb715c95275eb00ee1a9f4114
RingZero <mattia.paccamiccio@gmail.com>
2024-06-13 15:57:29 +0200

    segalang in progress

--- lib/segalang/memory.ml
+++ lib/segalang/memory.ml
@@ -2,7 +2,32 @@ module VM = Varmemory.VarMemory
 module OS = Operandstack
 module LS = Labelstack
 
-type t = { vm : VM.t; opsk : OS.t; lsk : LS.t }
+type ms = { vm : VM.t; opsk : OS.t; lsk : LS.t }
+type t = Def of ms | Bot
 
-let widen _ _ = failwith ""
-let join _ _ = failwith ""
+(*monadic operators*)
+let return x = Def x
+
+let bind (x, y) op =
+  match (x, y) with
+  | Bot, Bot -> Bot
+  | Def a, Bot | Bot, Def a -> Def a
+  | Def a, Def b -> op a b
+
+let ( >>= ) = bind
+
+(*lowlevel ops*)
+let lowlevel_widen m1 m2 =
+  { vm = m1.vm; opsk = OS.widen m1.opsk m2.opsk; lsk = m1.lsk }
+(*lsk is assumed to be the same on flow merges*)
+
+let lowlevel_lub m1 m2 =
+  { vm = m1.vm; opsk = OS.lub m1.opsk m2.opsk; lsk = m1.lsk }
+(*lsk is assumed to be the same on flow merges*)
+
+(*magic*)
+let widen m1 m2 = (m1, m2) >>= fun a b -> return (lowlevel_widen a b)
+let join m1 m2 = (m1, m2) >>= fun a b -> return (lowlevel_lub a b)
+let leq _ _ : bool = failwith ""
+let eq _ _ : bool = failwith ""
+let le m1 m2 = leq m1 m2 && not (eq m1 m2)
