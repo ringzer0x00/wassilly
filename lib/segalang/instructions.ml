54376cd0fb0f9ed6263c18d6d0ddd30a81d30f09
RingZero <mattia.paccamiccio@gmail.com>
2024-06-20 17:04:24 +0200

    lambdaed all of math instrs

--- lib/segalang/instructions.ml
+++ lib/segalang/instructions.ml
@@ -1,4 +1,5 @@
 open Memory
+open Lowlevel_instrs
 
 let bind x op = match x with Bot -> Bot | Def a -> op a
 let ( >>= ) = bind
@@ -26,56 +27,22 @@ let const_val _v prec =
       opsk = Operandstack.push_ops [ Value.of_int32 _v ] d.opsk;
     }
 
-let mul_expr (ad : VM.aprondomain) (l : Value.value) (r : Value.value) =
-  let exp =
-    Apronext.Texprext.binary Apronext.Texprext.Mul
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval l))
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval r))
-  in
-  Apronext.Abstractext.bound_texpr Apronext.Apol.man ad exp
-
 let mul prec =
   prec >>= fun d ->
   let opsk' = Operandstack.binop d.opsk (fun x y -> mul_expr d.vm.ad x y) in
   return { vm = d.vm; lsk = d.lsk; opsk = opsk' }
 
-let add_expr (ad : VM.aprondomain) (l : Value.value) (r : Value.value) =
-  let exp =
-    Apronext.Texprext.binary Apronext.Texprext.Add
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval l))
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval r))
-  in
-  Apronext.Abstractext.bound_texpr Apronext.Apol.man ad exp
-
 let add prec =
   prec >>= fun d ->
-  let add = add_expr d.vm.ad in
-  let opsk' = Operandstack.binop d.opsk add in
+  let opsk' = Operandstack.binop d.opsk (fun x y -> add_expr d.vm.ad x y) in
   return { vm = d.vm; lsk = d.lsk; opsk = opsk' }
-
-let sub_expr (ad : VM.aprondomain) (l : Value.value) (r : Value.value) =
-  let exp =
-    Apronext.Texprext.binary Apronext.Texprext.Sub
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval l))
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval r))
-  in
-  Apronext.Abstractext.bound_texpr Apronext.Apol.man ad exp
 
 let sub prec =
   prec >>= fun d ->
-  let sub = sub_expr d.vm.ad in
-  let opsk' = Operandstack.binop d.opsk sub in
+  let opsk' = Operandstack.binop d.opsk (fun x y -> sub_expr d.vm.ad x y) in
   return { vm = d.vm; lsk = d.lsk; opsk = opsk' }
-
-let neg_expr (ad : VM.aprondomain) (v : Value.value) =
-  let exp =
-    Apronext.Texprext.unary Apronext.Texprext.Neg
-      (Apronext.Texprext.cst ad.env (Apron.Coeff.Interval v))
-  in
-  Apronext.Abstractext.bound_texpr Apronext.Apol.man ad exp
 
 let neg prec =
   prec >>= fun d ->
-  let neg = neg_expr d.vm.ad in
-  let opsk' = Operandstack.unop d.opsk neg in
+  let opsk' = Operandstack.unop d.opsk (fun x -> neg_expr d.vm.ad x) in
   return { vm = d.vm; lsk = d.lsk; opsk = opsk' }
