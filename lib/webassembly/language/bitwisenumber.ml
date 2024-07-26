b07bdd31d6709d183be3af70f3c35fe45eae043b
RingZero <mattia.paccamiccio@gmail.com>
2024-07-26 13:58:35 +0200

    split of bitarray into array of bytes

--- lib/webassembly/language/bitwisenumber.ml
+++ lib/webassembly/language/bitwisenumber.ml
@@ -16,3 +16,95 @@ let of_interval interval type_ =
   | F64Type ->
       tuple_appl Int64.bits_of_float val_
       |> tuple_appl s_int64_to_binary_array_twos_complement_msb
+
+let split_in_bytes (a : int array) =
+  let l = Array.length a / 8 in
+  let _res = Array.make l (Array.make 8 0) in
+  Array.iteri
+    (fun i x ->
+      let byte_pos = i / 8 in
+      let pos_in_byte = i mod 8 in
+      _res.(byte_pos).(pos_in_byte) <- x)
+    a;
+  _res
+
+(*
+let x =
+  Language.Bitwisenumber.split_in_bytes
+    [|
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+      1;
+      2;
+      3;
+      4;
+      5;
+      6;
+      7;
+      8;
+    |]
+
+let () =
+  Array.iter
+    (fun x ->
+      Printf.printf "[";
+      Array.iter (fun x -> Printf.printf "%i; " x) x;
+      Printf.printf "]\n")
+    x;
+  Printf.printf "%i" (Array.length x)
+(*let _ = WASM.ops*)
+*)
--- test/test_chaotic_implicitgraph.ml
+++ test/test_chaotic_implicitgraph.ml
@@ -5,4 +5,5 @@ module WASM = Test_wasm
 (*open OUnit2
   let _ = run_test_tt_main MON.all
     let _ = run_test_tt_main SLT.all*)
+
 let _ = WASM.ops
