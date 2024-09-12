3e6d71dbc5c22bb2e4adc4a0c4041fb83f25017e
RingZero <mattia.paccamiccio@gmail.com>
2024-09-12 12:18:47 +0200

    instance wip

--- lib/webassembly/memories/instance.ml
+++ lib/webassembly/memories/instance.ml
@@ -4,7 +4,7 @@
    mems -> | ImportedMemSpec of .. | Mem
    tables -> | ImportedTableSpec of .. | Table
 *)
-type spec = string
+type spec = string * string
 
 (* -- *)
 type definedFunc = Wasm.Ast.func
@@ -17,13 +17,10 @@ type func = Func of definedFunc | ImportedFunc of spec
 type glob = Glob of definedGlob | ImportedGlob of spec
 type mem = Mem of memInst | ImportedMem of spec
 type table = Table of tableInst | ImportedTable of spec
-
-let x = ImportedTable "2"
+type impordesc = Wasm.Ast.import_desc'
 
 (*--*)
 type mod_ = Wasm.Ast.module_
-
-let instantiate_module (m : mod_) = m
 
 type instance = {
   types : Wasm.Ast.type_ list;
@@ -35,3 +32,32 @@ type instance = {
   datas : Wasm.Ast.data_segment list;
       (*exports : export list; ~ maybe functions should be tagged?*)
 }
+
+let imported_globs (i : Wasm.Ast.import list) =
+  let filter (x : Wasm.Ast.import) =
+    match x.it.idesc.it with GlobalImport _ -> true | _ -> false
+  in
+  List.filter filter i
+  |> List.map (fun (_x : Wasm.Ast.import) -> ImportedGlob ("name", "TOP"))
+
+let mkglobs gs_internal imports =
+  let gs_imported = imported_globs imports in
+  let gs_internal' =
+    List.map (fun (x : Wasm.Ast.global) -> Glob x) gs_internal
+  in
+  gs_imported @ gs_internal'
+
+let instantiate_module (m : mod_) : instance =
+  let m = m.it in
+  let types = m.types in
+  let elems = m.elems in
+  let datas = m.datas in
+  {
+    types;
+    globals = mkglobs m.globals m.imports;
+    tables = [];
+    memories = [];
+    funcs = [];
+    elems;
+    datas;
+  }
