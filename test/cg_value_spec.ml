2620809d0664ba8a3ee9ca15813002bd2c4b530b
RingZero <mattia.paccamiccio@gmail.com>
2024-10-07 10:37:34 +0200

    absolute value

--- test/cg_value_spec.ml
+++ test/cg_value_spec.ml
@@ -18,3 +18,15 @@ let%test "MATTIA-fib-one-external" =
       (Apronext.Scalarext.of_infty 1)
   in
   got g = [ (0, 0); (1, 0) ] && Apronext.Intervalext.cmp val_conc one_pinf = 0
+
+let%test "MATTIA-abs-val-external" =
+  Interpreter.Eval.cg := Fixpoint.Callgraph.CallGraph.phi;
+  let m, g = p "MATTIA-abs-val-external" in
+  let top_stack = Memories.Memorystate.peek_operand m |> List.hd in
+  let val_conc = Memories.Memorystate.operand_as_interval top_stack m in
+  let one_pinf =
+    Apronext.Intervalext.of_scalar
+      (Apronext.Scalarext.of_int 42)
+      (Apronext.Scalarext.of_int 42)
+  in
+  got g = [ (1, 0) ] && Apronext.Intervalext.cmp val_conc one_pinf = 0
---
+++ test/wasm/microbenchmarks/MATTIA-abs-val-external/main.wat
@@ -0,0 +1,14 @@
+;; Only direct calls. Simplest case.
+;; Entry point: $main
+;; Fully precise call graph (1 edge):
+;;   $main -> $reachable
+;; Reachable functions:
+;;   $main (by construction, if it's the entry point)
+;;   $reachable
+(module
+    (import "host" "absval" (func $absval (param i32) (result i32)))
+    (func $main (export "main") (result i32)
+        i32.const 42
+        call $absval
+    )
+)
