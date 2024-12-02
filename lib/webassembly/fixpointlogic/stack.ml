ff19af1dbec56a9762c9859068968e0bcbc1aad0
RingZero <mattia.paccamiccio@gmail.com>
2024-12-02 19:13:42 +0100

    modifica a call in stack, attenzione

--- lib/webassembly/fixpointlogic/stack.ml
+++ lib/webassembly/fixpointlogic/stack.ml
@@ -8,11 +8,12 @@ module Stack = struct
 
   let call_in_stack (env, expr) stack =
     match find_opt expr stack with
-    | Some envStack -> Memory.eq env envStack
+    | Some envStack -> Memory.leq env envStack (* env = envStack*)
     | None -> false
 
   let expr_in_stack e s =
     exists
-      (fun xpr _ -> if List.equal (fun a b -> a = b) e xpr then true else false)
+      (fun xpr _ ->
+        (*if List.equal (fun a b -> a = b) e xpr then true else false*) e = xpr)
       s
 end
