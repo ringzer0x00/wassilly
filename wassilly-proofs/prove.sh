71b41f06bde387b227381d4e644fd477ec4294d4
ringzer0x00 <mattia.paccamiccio@gmail.com>
2025-05-14 14:33:20 +0200

    boh

--- wassilly-proofs/prove.sh
+++ wassilly-proofs/prove.sh
@@ -1,9 +1,9 @@
 lean prova.lean
 if [ $? -eq 0 ]
 then
-	echo "Yay! Proofs compiled correctly!"
+	echo "\e[32mYay! Proofs compiled correctly!\e[0m"
 	exit 0
 else
-	echo "Proofs gave errors!!! :("
+	echo "\e[31mProofs gave errors!!! :(\e[0m"
 	exit 1
 fi
